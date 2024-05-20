aac62512db8c399aed163b2fa8bbc1914dff87e3
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-05-20 11:15:33 +0800

    style: fix alu.v code style

--- alu.v
+++ alu.v
@@ -20,24 +20,24 @@ module alu(A, B, ALUOp, C, Zero, PC);
          `ALUOp_lui: C = B;
          `ALUOp_auipc: C = PC + B;
          `ALUOp_add: C = A + B;
-         `ALUOp_sub:C = A - B;
-         `ALUOp_bne:C = { 31'b0, (A == B) };
-         `ALUOp_blt:C = { 31'b0, (A >= B) };
-         `ALUOp_bge:C = { 31'b0 , (A < B) };
-         `ALUOp_bltu:C = { 31'b0, ($unsigned(A) >= $unsigned(B)) };
-         `ALUOp_bgeu:C = { 31'b0, ($unsigned(A) < $unsigned(B))};
-         `ALUOp_slt:C={ 31'b0, (A < B)};
-         `ALUOp_sltu:C={ 31'b0, ($unsigned(A) < $unsigned(B))};
-         `ALUOp_xor:C = A ^ B;
-         `ALUOp_or:C = A | B;
-         `ALUOp_and:C = A & B;
-         `ALUOp_sll:C = A << B;
-         `ALUOp_srl:C = A >> B;
-         `ALUOp_sra:C = A >>> B;
+         `ALUOp_sub: C = A - B;
+         `ALUOp_bne: C = { 31'b0, (A == B) };
+         `ALUOp_blt: C = { 31'b0, (A >= B) };
+         `ALUOp_bge: C = { 31'b0 , (A < B) };
+         `ALUOp_bltu: C = { 31'b0, ($unsigned(A) >= $unsigned(B)) };
+         `ALUOp_bgeu: C = { 31'b0, ($unsigned(A) < $unsigned(B))};
+         `ALUOp_slt: C={ 31'b0, (A < B)};
+         `ALUOp_sltu: C={ 31'b0, ($unsigned(A) < $unsigned(B))};
+         `ALUOp_xor: C = A ^ B;
+         `ALUOp_or: C = A | B;
+         `ALUOp_and: C = A & B;
+         `ALUOp_sll: C = A << B;
+         `ALUOp_srl: C = A >> B;
+         `ALUOp_sra: C = A >>> B;
       endcase
    end
    
    assign Zero = (C == 32'b0);
-
+   
 endmodule
     
