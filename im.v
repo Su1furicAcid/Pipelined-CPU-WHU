7c39da1fcf8dc40878c75850030b74f25826b15e
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-05-21 22:36:05 +0800

    fix: change ROM to RAM in im.v

--- im.v
+++ im.v
@@ -1,6 +1,9 @@
+// Editor: SunAo
+// LastEditTime: 2024/5/21
+
 // instruction memory
 module im(input [8:2] addr, output [31:0] dout);
-  reg  [31:0] ROM[127:0];
+  reg  [31:0] RAM[127:0];
   // read
-  assign dout = ROM[addr]; 
+  assign dout = RAM[addr]; 
 endmodule  
