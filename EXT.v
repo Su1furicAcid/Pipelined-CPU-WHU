b7ecba0c0d13c0bc7840d9d23156baa1a8a4716e
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-05-21 22:33:54 +0800

    style: fix EXT.v code style

--- EXT.v
+++ EXT.v
@@ -1,25 +1,47 @@
+// Editor: SunAo
+// LastEditTime: 2024/5/21
+
 `include "ctrl_encode_def.v"
+
+// iimm_shamt: immediate for shift operations
+// funct7[4:0] imm[4:0] rs1[4:0] funct3[2:0] rd[4:0] opcode[6:0]
+
+// iimm: immediate for I-type instructions, expect shift operations
+// imm[11:0] rs1[4:0] funct3[2:0] rd[4:0] opcode[6:0]
+
+// simm: immediate for S-type instructions
+// imm[11:5] rs2[4:0] rs1[4:0] funct3[2:0] opcode[6:0]
+
+// sbimm: immediate for SB-type instructions
+// imm[12] imm[10:5] rs2[4:0] rs1[4:0] funct3[2:0] opcode[6:0]
+
+// uimm: immediate for U-type instructions
+// imm[31:12] rd[4:0] opcode[6:0]
+
+// ujimm: immediate for UJ-type instructions
+// imm[20] imm[10:1] imm[11] imm[19:12] rd[4:0] opcode[6:0]
+
+// EXTOp: control signal for extension, identify the type of immediate
+// immout: output 32-bit immediate
 module EXT( 
 	input [4:0] iimm_shamt,
-    	input	[11:0]			iimm, //instr[31:20], 12 bits
-	input	[11:0]			simm, //instr[31:25, 11:7], 12 bits
-	input	[11:0]			bimm, //instrD[31], instrD[7], instrD[30:25], instrD[11:8], 12 bits
-	input	[19:0]			uimm,
-	input	[19:0]			jimm,
-	input	[5:0]			EXTOp,
-
-	output	reg [31:0] 	       immout);
+  input	[11:0] iimm,
+	input	[11:0] simm,
+	input	[11:0] sbimm,
+	input	[19:0] uimm,
+	input	[19:0] ujimm,
+	input	[5:0]	EXTOp,
+	output reg [31:0] immout
+);
    
 always  @(*)
 	 case (EXTOp)
-		`EXT_CTRL_ITYPE_SHAMT:   immout<={27'b0,iimm_shamt[4:0]};
-		`EXT_CTRL_ITYPE:	immout <= {{{32-12}{iimm[11]}}, iimm[11:0]};
-		`EXT_CTRL_STYPE:	immout <= {{{32-12}{simm[11]}}, simm[11:0]};
-		`EXT_CTRL_BTYPE:        immout <= {{{32-13}{bimm[11]}}, bimm[11:0], 1'b0};
-		`EXT_CTRL_UTYPE:	immout <= {uimm[19:0], 12'b0}; //???????????12??0
-		`EXT_CTRL_JTYPE:	immout <= {{{32-21}{jimm[19]}}, jimm[19:0], 1'b0};
-		default:	        immout <= 32'b0;
+		`EXT_CTRL_ITYPE_SHAMT: immout <= { 27'b0, iimm_shamt[4:0] };
+		`EXT_CTRL_ITYPE: immout <= { { { 32 - 12 }{ iimm[11] } }, iimm[11:0] };
+		`EXT_CTRL_STYPE: immout <= { { { 32 - 12 }{ simm[11] } }, simm[11:0] };
+		`EXT_CTRL_BTYPE: immout <= { { { 32 - 13 }{ sbimm[11] } }, sbimm[11:0], 1'b0};
+		`EXT_CTRL_UTYPE: immout <= { uimm[19:0], 12'b0 }; 
+		`EXT_CTRL_JTYPE: immout <= { { { 32 - 21 }{ ujimm[19] }}, ujimm[19:0], 1'b0};
+		default: immout <= 32'b0;
 	 endcase
-
-       
 endmodule
