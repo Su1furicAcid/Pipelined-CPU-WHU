bdd42e2f20008d38d05a67530127fc22d12f6b42
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-05-22 11:34:41 +0800

    fix: larger instr addr width

--- im.v
+++ im.v
@@ -2,8 +2,8 @@
 // LastEditTime: 2024/5/21
 
 // instruction memory
-module im(input [8:2] addr, output [31:0] dout);
+module im(input [31:0] addr, output [31:0] dout);
   reg  [31:0] RAM[127:0];
   // read
-  assign dout = RAM[addr]; 
+  assign dout = RAM[addr[8:2]]; 
 endmodule  
