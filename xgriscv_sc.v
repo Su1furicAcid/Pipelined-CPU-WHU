e8dd54a08e642379ebbe23e47eeeeb79fccfc8a9
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-05-22 11:10:11 +0800

    refactor: refactor sc

--- xgriscv_sc.v
+++ xgriscv_sc.v
@@ -1,42 +1,19 @@
-module xgriscv_sc(clk, rstn, pcW);
-   input          clk;
-   input          rstn;
-   output [31:0]  pcW;
-   
-   wire [31:0]    instr;
-   wire [31:0]    PC;
-   wire           MemWrite;
-   wire [31:0]    dm_addr, dm_din, dm_dout;
-   
-   wire rst = ~rstn;
-       
-  // instantiation of single-cycle CPU   
-   SCPU U_SCPU(
-         .clk(clk),                 // input:  cpu clock
-         .reset(rst),                 // input:  reset
-         .inst_in(instr),             // input:  instruction
-         .Data_in(dm_dout),        // input:  data to cpu  
-         .mem_w(MemWrite),       // output: memory write signal
-         .PC_out(PC),                   // output: PC
-         .Addr_out(dm_addr),          // output: address from cpu to memory
-         .Data_out(dm_din),        // output: data from cpu to memory
-         .pcW(pcW)        // output: register data
-         );
-         
-  // instantiation of data memory  
-   dm U_dmem(
-         .clk(clk),           // input:  cpu clock
-         .DMWr(MemWrite),     // input:  ram write
-         .addr(dm_addr[8:2]), // input:  ram address
-         .din(dm_din),        // input:  data to ram
-         .dout(dm_dout)       // output: data from ram
-         );
-         
-  // instantiation of intruction memory (used for simulation)
-   im U_imem ( 
-      .addr(PC[8:2]),     // input:  rom address
-      .dout(instr)        // output: instruction
-   );
-        
-endmodule
+module xgriscv_sc(clk, rst, pcNow);
 
+    input         clk, rst;
+    output [31:0] pcNow;
+   
+    wire [31:0] instruction;
+    wire [31:0] PC;
+    wire        MemWrite;
+    wire [31:0] dm_addr, dm_din, dm_dout;
+
+    SCPU CPU_UNIT(
+        .clk(clk), .rst(rst), .Inst_in(instruction), .Data_in(dm_dout), .MemWrite(MemWrite),
+        .PC_out(PC), .Addr_out(dm_addr), .Data_out(dm_din), .pcNow(pcNow)
+    );
+
+    im U_imem(.RA(PC), .RD(instruction));
+    dm U_dmem(.clk(clk), .MemWrite(MemWrite), .Addr(dm_addr), .WD(dm_din), .RD(dm_dout), .PC(PC));
+
+endmodule
