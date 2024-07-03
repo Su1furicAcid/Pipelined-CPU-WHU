517f104c7fd1b5b334b0c617d2f5a76bf309d06e
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-07-03 13:09:52 +0800

    fix: RF

--- RF.v
+++ RF.v
@@ -25,7 +25,7 @@ module RF(
   integer i;
 
   // write data
-  always @(negedge clk or posedge rst)
+  always @(posedge clk or posedge rst)
     // reset 
     if (rst) begin
       for (i = 0; i < 32; i = i + 1)
