87805e90f64dd4315f7c6879736b557c6a10bdec
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-07-04 02:55:59 +0800

    chore: delete dm / im

--- im.v
+++
@@ -1,9 +0,0 @@
-// Editor: SunAo
-// LastEditTime: 2024/5/21
-
-// instruction memory
-module im(input [31:0] addr, output [31:0] dout);
-  reg  [31:0] RAM[127:0];
-  // read
-  assign dout = RAM[addr[8:2]]; 
-endmodule  
