4853c323f0155d3bd20752d7dbc21b6d5ba28f32
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-07-10 12:16:55 +0800

    chore: why??????

--- stage_reg.v
+++ stage_reg.v
@@ -1,11 +1,22 @@
 module StageReg (
-  input Clk, Rst, flush,
-  input [31:0] in0, in1, in2, in3, in4, in5, in6, in7,
+  input wire Clk, 
+  input wire Rst, 
+  input wire flush,
+  input wire [31:0] in0, in1, in2, in3, in4, in5, in6, in7,
   output reg [31:0] out0, out1, out2, out3, out4, out5, out6, out7
 );
 
   always @(posedge Clk or posedge Rst) begin
-    if (Rst | flush) begin
+    if (Rst) begin
+      out0 <= 32'h0000_0000;
+      out1 <= 32'h0000_0000;
+      out2 <= 32'h0000_0000;
+      out3 <= 32'h0000_0000;
+      out4 <= 32'h0000_0000;
+      out5 <= 32'h0000_0000;
+      out6 <= 32'h0000_0000;
+      out7 <= 32'h0000_0000;
+    end else if (flush) begin
       out0 <= 32'h0000_0000;
       out1 <= 32'h0000_0000;
       out2 <= 32'h0000_0000;
