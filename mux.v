90fa422d4f8157760b92bfb4a528d9deb40401d9
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2024-07-12 17:09:17 +0800

    chore

--- mux.v
+++ mux.v
@@ -31,22 +31,3 @@ module mux3(
     endcase
   end
 endmodule
-
-module mux4(
-  input [1:0] sel,
-  input [31:0] in0,
-  input [31:0] in1,
-  input [31:0] in2,
-  input [31:0] in3,
-  output reg [31:0] out
-);
-  always @(*) begin
-    case (sel)
-      2'b00: out = in0;
-      2'b01: out = in1;
-      2'b10: out = in2;
-      2'b11: out = in3;
-      default: out = 0;
-    endcase
-  end
-endmodule
